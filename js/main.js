21122accb0dc189a3e01bdda6a9f8876e5463ba0
Christopher Lo <celo@ucsd.edu>
2016-03-24 00:16:27 -0700

    update resume and portfolio layout/info

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$("#modal01").animatedModal({
+    modalTarget:'modal1',
+    animatedIn:'slideInRight',
+    animatedOut:'slideOutLeft',
+    color:'#222',
+});
+
+$("#modal02").animatedModal({
+    modalTarget:'modal2',
+    animatedIn:'slideInRight',
+    animatedOut:'slideOutLeft',
+    color:'#222',
+});
+
+$("#modal03").animatedModal({
+    modalTarget:'modal3',
+    animatedIn:'slideInRight',
+    animatedOut:'slideOutLeft',
+    color:'#222',
+});
+
+$("#modal04").animatedModal({
+    modalTarget:'modal4',
+    animatedIn:'slideInRight',
+    animatedOut:'slideOutLeft',
+    color:'#222',
+});
