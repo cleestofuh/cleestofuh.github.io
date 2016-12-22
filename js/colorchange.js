fb370f5ab29db949df3f96e60d594a6b5f007fbd
Christopher Lo <celo@ucsd.edu>
2016-12-22 03:29:00 -0800

    portfolio v5 revamped yo"

---
+++ js/colorchange.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    $("#logo").hover(function(){
+        $(".st1").css("fill", "#444444");
+        }, function(){
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#grape").hover(function(){
+        $(".st0").css("stroke", "#c399d1");
+        $(".st1").css("stroke", "#c399d1");
+        $(".st1").css("fill", "#c399d1");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#prch").hover(function(){
+        $(".st0").css("stroke", "#39b54a");
+        $(".st1").css("stroke", "#39b54a");
+        $(".st1").css("fill", "#39b54a");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#studybud").hover(function(){
+        $(".st0").css("stroke", "#1F618D");
+        $(".st1").css("stroke", "#1F618D");
+        $(".st1").css("fill", "#1F618D");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#suhdude").hover(function(){
+        $(".st0").css("stroke", "#4d62a7");
+        $(".st1").css("stroke", "#4d62a7");
+        $(".st1").css("fill", "#4d62a7");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#brewngo").hover(function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#444444");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#kf").hover(function(){
+        $(".st0").css("stroke", "#f48516");
+        $(".st1").css("stroke", "#f48516");
+        $(".st1").css("fill", "#f48516");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#banana").hover(function(){
+        $(".st0").css("stroke", "#e9d52c");
+        $(".st1").css("stroke", "#e9d52c");
+        $(".st1").css("fill", "#e9d52c");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+    $("#christmas").hover(function(){
+        $(".st0").css("stroke", "#d44644");
+        $(".st1").css("stroke", "#d44644");
+        $(".st1").css("fill", "#d44644");
+        }, function(){
+        $(".st0").css("stroke", "#444444");
+        $(".st1").css("stroke", "#444444");
+        $(".st1").css("fill", "#ffffff");
+    });
+});
